02e08b92fcefdb0d7c41374721dedf2b5c9efbc1
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 14:15:55 -0600

    create a App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>welcome to collabApp</div>
+    <main>
+      <section>
+        <div>
+          {" "}
+          <h2>navbar</h2>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h1>please search a group</h1>
+        </div>
+
+        <div>
+          <h1>lists of random pick projects</h1> 
+        </div>
+      </section>
+      <section><h1>footer</h1></section>
+    </main>
   );
 }
 
