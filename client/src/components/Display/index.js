2295808792befeaa023d840173f2692d0af03890
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 15:59:30 -0600

    create Display components

---
+++ client/src/components/Display/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../Button";
+
+
+export default function Display(props) {
+return (
+  <article>
+    <Button>find a group</Button>
+    <Button>create a project</Button>
+   <div>list of projects</div>
+  </article>
+)
+} 
