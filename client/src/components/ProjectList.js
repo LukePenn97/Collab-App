b0bbd395382d731188505d37b9df5b769320f254
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 20:51:56 -0600

    create ProjectList component

---
+++ client/src/components/ProjectList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ProjectListItem from "./ProjectListItem"
+
+
+export default function ProjectList(props) {
+  // console.log("hi")
+  // console.log(props.projects)
+
+  const projects = props.projects.map((project,index) => {
+    return (
+      <ProjectListItem 
+      key={index}
+      name = {project.name}
+      lead = {project.lead}
+      description = {project.description}
+      />
+    )
+  })
+
+  return(
+    <ul>
+      {projects}
+    </ul>
+  )
+} 
+
