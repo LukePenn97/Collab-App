ce20faa6adfb403286922501eaf39c6cf98c7ce3
Luke Penner <lukepenner@hotmail.ca>
2021-06-05 22:55:39 -0700

    created goals table and migration

---
+++ CollapApp-api/models/goals.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Goals extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Goals.init({
+    projectId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Goals',
+  });
+  return Goals;
+};
