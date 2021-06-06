2bddf4f1b5a1091cac4e770724faafd44bdadc90
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 20:52:26 -0600

    create ProjectListitem component

---
+++ client/src/components/ProjectListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "components/DayListItem.scss";
+
+// const classnames = require('classnames');
+
+export default function DayListItem(props) {
+  // const itemClass = classnames("day-list__item", {
+  //   "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0
+  // })
+
+  
+
+  return (
+    <li>
+      <h2 >{props.name}</h2>
+      <h3 >{props.lead}</h3>
+      <p >{props.description}</p>
+    </li>
+  );
+}
