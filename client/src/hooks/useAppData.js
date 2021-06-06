a6980e722069390eabe8062e71900a03c1de143e
Jessie-p05 <jxpei05@gmail.com>
2021-06-06 12:02:19 -0600

    create useeAppDate hook, set the state

---
+++ client/src/hooks/useAppData.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+// const axios = require('axios').default;
+
+
+export default function useApplicationData() {
+  const fakeProjects = [
+    {
+      id: 1,
+      name: "react-app",
+      lead: "Amy",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+    {
+      id: 2,
+      name: "Ruby",
+      lead: "Bell",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
+  ];
+
+
+  const [state, setState] = useState({
+    project:null,
+    projects: fakeProjects
+  });
+  // fetch the initial data in database;
+  // useEffect(() => {
+  //   axios.defaults.baseURL = "http://localhost:8001";
+  //   Promise.all([
+  //     axios.get('/api/days'),
+  //     axios.get('/api/appointments'),
+  //     axios.get('/api/interviewers')
+  //   ]).then((all) => {
+  //     setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data, spots: all[0].data.spots }));
+  //   })
+  // }, []);
+
+  //update project state when user click on a project
+  const setProject = project => setState({ ...state, project });
+
+  //update projects state when user do a search
+  const setProjects = projects => setState({...state, projects})
+
+
+
+  
+  
+  return { state, setProject, setProjects }
+}
