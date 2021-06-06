5bb70ac5e1690a5292ae080cb7299b034a5b2a48
Arvin Radan <radanarvin@gmail.com>
2021-06-06 16:33:49 -0700

    Correct the users.js in routes folder

--- CollabApp-api/routes/users.js
+++ CollabApp-api/routes/users.js
@@ -1,18 +1,65 @@
 const router = require("express").Router();
-const db = require('../config/database')
-const User = require('../models/user.js')
-const Project = require('../models/project.js')
+const {sequelize, User } = require('../models');
 
 //get user list
 router.get('/', (req, res) =>
 User.findAll()
   .then(users => {
     console.log("Users:", users);
-    Project.findAll()
-    .then(projects => console.log("projects:", projects))
-    
     res.sendStatus(200);
   })
   .catch(err => console.log("Error:"+ err)));
+
+//get user by ID
+router.get('/:id', (req, res) =>
+User.findByPk(req.params.id)
+  .then(users => {
+    console.log("Users:", users.dataValues);
+    res.sendStatus(200);
+  })
+  .catch(err => console.log("Error:"+ err)));
+
+//Post (create) new user
+router.post('/', async(req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  try {
+      const user = await User.create({firstName, lastName, email, password})
+      return res.json(user)
+  } catch (err) {
+      console.log(err)
+      return res.status(500).json(err);
+  }
+});
+
+//patch (update) user's attr.
+router.patch('/:id', async(req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  try {
+      const user = await User.update({firstName, lastName, email, password},
+        {where: {
+          id: req.params.id
+        }})
+      return res.json(user)
+  } catch (err) {
+      console.log(err)
+      return res.status(500).json(err);
+  }
+});
+
+//Delete a user
+router.delete('/:id', async(req, res) => {
+    User.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then (data => {
+    console.log("Deleting was successfull for user ID:", data);
+    res.sendStatus(200);
+  })
+  .catch ((err) => {
+      console.log(err)
+      return res.status(500).json(err);
+    });
+});
 
 module.exports = router;
