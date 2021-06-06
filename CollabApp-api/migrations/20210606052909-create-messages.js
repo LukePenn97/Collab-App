bee1269024dbc2caaa43d7fe85a85a9f00ccc22f
Luke Penner <lukepenner@hotmail.ca>
2021-06-06 14:41:23 -0700

    table names and keys capitalized

--- CollabApp-api/migrations/20210606052909-create-messages.js
+++ CollabApp-api/migrations/20210606052909-create-messages.js
@@ -8,10 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      userId: {
+      UserId: {
         type: Sequelize.INTEGER
       },
-      projectId: {
+      ProjectId: {
         type: Sequelize.INTEGER
       },
       message: {
