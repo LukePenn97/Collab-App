a24bf42d225045c991f26674adda9c96770a90dc
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 23:00:20 -0600

    add button on navbar to go back to homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
+// import "./App.css";
 
 import Display from "./Display";
 import MatchProject from "./MatchProject";
 
 import useVisualMode from "../hooks/useVisualMode";
+import Button from "./Button";
 
 function App() {
   const projects = [
@@ -17,18 +19,25 @@ function App() {
     },
     {
       id: 2,
-      name: "react-app",
+      name: "Ruby",
       lead: "Bell",
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     },
   ];
+  // this.state = projects
   const DISPLAY = "DISPLAY";
   const MATCH = "MATCH";
   const { mode, transition, back } = useVisualMode(DISPLAY);
+  // const [state, setState] = useState({
+  //   projects: [],
+  // });
 
   function onMatch() {
     transition(MATCH);
+  }
+  function backToHome() {
+    transition(DISPLAY);
   }
 
   return (
@@ -36,14 +45,12 @@ function App() {
       <section>
         <div>
           <h2>navbar</h2>
+          <Button onClick={backToHome}>home</Button>
         </div>
       </section>
 
       <section>
-        {mode === DISPLAY && <Display 
-        projects = {projects} 
-        onMatch = {onMatch}
-        />}
+        {mode === DISPLAY && <Display projects={projects} onMatch={onMatch} />}
         {mode === MATCH && <MatchProject projects={projects.slice(1)} />}
       </section>
 
