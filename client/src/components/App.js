b54d351274db75d348a513159a7eb6f8a7a2684e
Jessie-p05 <jxpei05@gmail.com>
2021-06-05 20:51:32 -0600

    add some fake data for testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,37 @@
+import React from "react";
 import "./App.css";
+import Display from "./Display"
 
 function App() {
+
+ const projects = [
+   { id:1,
+     name: 'react-app',
+     lead: "Amy",
+     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+   } ,
+  { id:2,
+     name: 'react-app',
+     lead: "Bell",
+     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+   } 
+  ]
+
   return (
     <main>
       <section>
         <div>
-          {" "}
           <h2>navbar</h2>
         </div>
       </section>
 
       <section>
-        <div>
-          <h1>please search a group</h1>
-        </div>
-
-        <div>
-          <h1>lists of random pick projects</h1> 
-        </div>
+        <Display 
+        projects = {projects}/>
       </section>
+
       <section><h1>footer</h1></section>
     </main>
   );
