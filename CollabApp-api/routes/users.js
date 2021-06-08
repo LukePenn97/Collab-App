e7dd3ed9b0fb4a10a10efdd248a612f7e771dd6b
Arvin Radan <radanarvin@gmail.com>
2021-06-08 10:03:22 -0700

    Add some user skill routes

--- CollabApp-api/routes/users.js
+++ CollabApp-api/routes/users.js
@@ -1,6 +1,36 @@
 const router = require("express").Router();
 const {sequelize, User, Skill, Users_Skills } = require('../models');
 
+
+//get skill list
+router.get('/skills', (req, res) =>
+Skill.findAll()
+  .then(skills => {
+    console.log("Skills:", skills);
+    res.json(skills);
+  })
+  .catch(err => console.log("Error:"+ err)));
+
+//get user's skills by ID
+router.get('/:id/skills', (req, res) =>
+Users_Skills.findAll({include: Skill, where: {userId: req.params.id}})
+  .then(skills => {
+    console.log("Users:", skills.dataValues);
+    return res.json(skills);
+  })
+  .catch(err => console.log("Error:"+ err))
+);
+
+//get user by ID
+router.get('/:id', (req, res) =>
+User.findByPk(req.params.id)
+  .then(users => {
+    console.log("Users:", users.dataValues);
+    return res.json(users);
+  })
+  .catch(err => console.log("Error:"+ err))
+);
+
 //get user list
 router.get('/', (req, res) =>
 User.findAll()
@@ -10,28 +40,23 @@ User.findAll()
   })
   .catch(err => console.log("Error:"+ err)));
 
-//get user by ID
-router.get('/:id', (req, res) =>
-User.findByPk(req.params.id)
-  .then(users => {
-    console.log("Users:", users.dataValues);
-    res.sendStatus(200);
-  })
-  .catch(err => console.log("Error:"+ err))
-);
-
 //patch (update) user's skills.
 router.post('/:id/skills', async(req, res) => {
-  const { javascript, phyton, react, ruby, css } = req.body
+  const myItems = Object.values({ item1, item2, item3, item4, item5 } = req.body);
+  const myId = [];
   try {
-      const userSkill = await Skill.create({javascript, phyton, react, ruby, css})
-      const  skillId = await Users_Skills.create({userId: req.params.id, skillId: userSkill.id})
-      return res.json(userSkill, skillId)
+    for(elem of myItems) {
+      skillId = await Skill.findOne({where: {
+        name: elem
+      }})
+      const thisId = await Users_Skills.create({userId: req.params.id, skillId})
+      myId.push(thisId);
+    }
+    return res.json(myId)
   } catch (err) {
       console.log(err)
       return res.status(500).json(err);
   }
-
 });
 
 //Post (create) new user
