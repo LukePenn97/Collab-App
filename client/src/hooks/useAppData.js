7ff909b0b2548f0b9c221e07355e89bcbfa6a02b
Jessie-p05 <jxpei05@gmail.com>
2021-06-08 21:41:45 -0600

    delete hard code data

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -3,24 +3,7 @@ const axios = require('axios').default;
 
 
 export default function useApplicationData() {
-  const fakeProjects = [
-    {
-      id: 1,
-      name: "react-app",
-      lead: "Amy",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-    {
-      id: 2,
-      name: "Ruby",
-      lead: "Bell",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-  ];
-
-
+  
   const [state, setState] = useState({
     user:null,
     project:null,
