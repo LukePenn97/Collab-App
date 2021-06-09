9f942bbe465782c449ecf57383397f4fbe3a911a
Jessie-p05 <jxpei05@gmail.com>
2021-06-08 21:43:54 -0600

    display the msg on chatroom

--- client/src/components/ChatRoom/index.js
+++ client/src/components/ChatRoom/index.js
@@ -1,31 +1,36 @@
 import React from "react";
 import Button from "../Button";
 import ProjectList from "../ProjectList";
+import { findUserById } from "../../helpers/selectors";
 
 export default function ChatRoom(props) {
+  const messages = props.project.project_messages.map((msg, index) => {
+    const user = findUserById(msg.UserId, props.users);
+
+    return (
+      <div>
+        <img src={user.photo} alt={user.firstName} />
+        <h5>
+          {user.firstName} {user.lastName} :
+        </h5>
+        <p>{msg.message}</p>
+      </div>
+    );
+  });
+
   return (
     <article>
-      {/* <ProjectList 
-    project={state.project} 
-    projects={props.projects}
-    pickAProject = {props.pickAProject}
-    /> */}
+      
       <div>
         <h1>------------------------chatBox---------------</h1>
-        <p>blahblahblahblahblahblah</p>
-        <p> blahblahblahblahblahblahblah</p>
-        <p>blahblahblah</p>
-        <p> blahblahblahblahblahblahblah</p>
-        <p> blahblahblahblahblahblahblahblah</p>
-        <p>blahblahblahblahblahblahblahblah</p>
-        <p>blahblahblahblah</p>
+
+        {messages}
       </div>
       <div>
-        <h2>{props.project.name}</h2> 
-        <h3 onClick ={()=> props.pickAUser(props)}>{props.project.lead}</h3> 
-        <p>{props.project.description}</p> 
-
-        </div>
+        <h2>{props.project.name}</h2>
+        <h3 onClick={() => props.pickAUser(props)}>{props.project.lead}</h3>
+        <p>{props.project.description}</p>
+      </div>
     </article>
   );
 }
--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,30 +1,26 @@
 import React from "react";
-import ProjectListItem from "./ProjectListItem"
-
+import ProjectListItem from "./ProjectListItem";
 
 export default function ProjectList(props) {
   // console.log("hi")
   // console.log(props.projects)
 
-  const projects = props.projects.map((project,index) => {
+  const projects = props.projects.map((project, index) => {
     return (
-      <ProjectListItem 
-      key={index}
-      name = {project.name}
-      imgUrl = {project.imgUrl}
-      projectLeadId = {project.projectLeadId}
-      description = {project.description}
-      project_users = {project.project_users}
-      pickAProject = {props.pickAProject}
-      pickAUser = {props.pickAUser}
+      <ProjectListItem
+        key={index}
+        name={project.name}
+        imgUrl={project.imgUrl}
+        projectLeadId={project.projectLeadId}
+        description={project.description}
+        project_skills={project.project_skills}
+        project_messages={project.project_messages}
+        project_users={project.project_users}
+        pickAProject={props.pickAProject}
+        pickAUser={props.pickAUser}
       />
-    )
-  })
+    );
+  });
 
-  return(
-    <ul>
-      {projects}
-    </ul>
-  )
-} 
-
+  return <ul>{projects}</ul>;
+}
