3ad19b04ad413cbfb494647b430966e9fda7d003
Jessie-p05 <jxpei05@gmail.com>
2021-06-13 19:18:52 -0600

    create a pop up form to add goal

--- client/src/components/ProjectDetail/index.js
+++ client/src/components/ProjectDetail/index.js
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import { findUserById } from "../../helpers/selectors";
+import axios from "axios";
+import { Container } from './Container';
+import './index.css'
 
 
 export default function ProjectDetail(props) {
-  const projectLead = findUserById(props.project.projectLeadId, props.project.project_users);
+  const projectLead = findUserById(props.project.projectLeadId, props.users);
+  // console.log("lead",projectLead)
+  const goalToChange = props.project.project_goals;
+  console.log("projectInState",goalToChange)
+  // console.log("projectstate!!!", props.project)
+  
+  // const [checkedState, setCheckedState] = useState(
+    //   new Array(goalToChange.length).fill(goalToChange.map(goal => goal.completedAt) ? true : false)
+    // );
+    
+    // const [total, setTotal] = useState(0);
+    
+    const handleOnChange = (position,goal) => {
+      // const updatedCheckedState = checkedState.map((item, index) =>
+      //   index === position ? !item : item
+      // );
+      
+      // setCheckedState(updatedCheckedState);
+      const changedGoalItem = {...goal, completedAt: goal.completedAt? null : new Date()}
+      // console.log("item",changedGoalItem)
+      // const changedGoal = goalToChange.map((goal,index) => 
+      //    || goal);
+      // console.log(position)
+      // console.log("beforeChange",goalToChange)
+    goalToChange[position] = changedGoalItem;
+     const changeProject = {...props.project, project_goals:goalToChange}
+     
+     
+     //  const projectToChange = changeProject;
+     const projectToChange = props.project;
+     const projectsToChange = props.projects;
+     
+     
+     
+     axios.patch(`http://localhost:5000/projects/${props.project.id}/${goal.id}`);
+     console.log("change",changeProject.project_goals);
+     console.log("state",props.project)
+     console.log("projects",props.projects)
+     
+    props.setState(prev => ({...prev, project:projectToChange, projects: projectsToChange}))
+    // console.log("projectstate", props.project)
+    // props.setProjects(prev => ({...prev}) )
+    
+  };
 
-  // const projectMembers = 
+
+  const triggerText = 'Add Goal';
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    // console.log(event.target.name.value);
+    // console.log(event.target.email.value);
+  return  axios.post(`http://localhost:5000/projects/${props.project.id}/addGoal`,{
+      name:event.target.name.value,
+      description:event.target.description.value,
+      startDate:event.target.startDate.value,
+      deadline:event.target.deadline.value,
+    }).then((body) => {
+      const goalsBeforeUpdate = props.project.project_goals;
+      goalsBeforeUpdate.push(body.data);
+      const projectWithNewGoal = {...props.project, project_goals: goalsBeforeUpdate};
+      const projectsWithNewGoal = props.projects;
+      props.setState(prev => ({...prev,project:projectWithNewGoal,projects:projectsWithNewGoal}));
+    })
+  };
+ 
 
   return (
     <article>
@@ -22,6 +87,49 @@ export default function ProjectDetail(props) {
       alt={props.project.name}
     />
       <Button onClick={()=>props.chatToAGroup(props.project.id)}>chatRoom</Button>
+      <div>
+      <h2>-------------------------Goals-----------------------</h2>
+
+      </div>
+      <div className="App">
+      <h3>Select Toppings</h3>
+      <ul className="toppings-list">
+        {props.project.project_goals.map((goal, index) => {
+          return (
+            <li key={index}>
+              <div className="toppings-list-item">
+                <div className="left-section">
+                  <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={goal.name}
+                    value={goal.name}
+                    checked={goal.completedAt}
+                    onChange={() => handleOnChange(index,goal)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{goal.name}</label>
+                </div>
+                <div className="right-section">{goal.description}</div> 
+                <div>deadline: {goal.deadline} </div>
+                <div>{goal.completedAt? (
+                  <div>
+                <span>completed at: </span> 
+                <p>{JSON.stringify(goal.completedAt)}</p> 
+               </div>
+                ):(
+                  null)}
+                  </div>
+              </div>
+            </li>
+          )})}
+      </ul>
+        <div>
+        <Container triggerText={triggerText} onSubmit={onSubmit} />
+        </div>
+    </div>
+  
+
+
     </article>
   );
 }
